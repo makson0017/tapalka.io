e3b852784131a79d607e580a80e00cc9585faf95
makson0017 <maksyasenev@gmail.com>
2024-10-15 12:30:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const circle = document.getElementById('circle');
+
+circle.addEventListener('click', () => {
+    // Вы можете добавить здесь любые действия при клике на круг
+    alert('Круг был нажат!');
+});
