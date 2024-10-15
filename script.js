e450110d3d36c0f7dc536ad840d2ce5c0525b388
makson0017 <maksyasenev@gmail.com>
2024-10-15 12:34:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const circle = document.getElementById('circle');
 
 circle.addEventListener('click', () => {
-    // Вы можете добавить здесь любые действия при клике на круг
     alert('Круг был нажат!');
 });
