e91d7b8a00522d052682e5118e9a67c8c22d35e7
makson0017 <maksyasenev@gmail.com>
2024-10-15 12:40:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const circle = document.getElementById('circle');
 
+document.addEventListener('mousemove', (event) => {
+    const x = event.clientX; // Получаем координату X курсора
+    const y = event.clientY; // Получаем координату Y курсора
+    circle.style.left = `${x}px`; // Устанавливаем позицию круга по X
+    circle.style.top = `${y}px`; // Устанавливаем позицию круга по Y
+});
+
 circle.addEventListener('click', () => {
     alert('Круг был нажат!');
 });
